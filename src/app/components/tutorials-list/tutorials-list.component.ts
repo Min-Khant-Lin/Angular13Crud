d8d5d60379b389a26f94deaa282dea5b2e3b6c3c
AltairEzio <unicolour35@gmail.com>
2022-01-21 14:26:46 +0900

    add-tutorial completed

---
+++ src/app/components/tutorials-list/tutorials-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Tutorial } from 'src/app/models/tutorial.model';
+import { TutorialService } from 'src/app/services/tutorial.service';
+
+@Component({
+  selector: 'app-tutorials-list',
+  templateUrl: './tutorials-list.component.html',
+  styleUrls: ['./tutorials-list.component.css']
+})
+export class TutorialsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
